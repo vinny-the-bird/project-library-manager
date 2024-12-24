618db393da53ae51e53e6353b4ea45e17285a516
Vincent-Jung <117348000+Vincent-Jung@users.noreply.github.com>
2024-12-24 13:31:31 +0100

    added button + basic function to simulate action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("running app...")
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton
 from PyQt5.QtGui import QIcon, QFont, QPixmap
 from PyQt5.QtCore import Qt
 
@@ -10,12 +10,12 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Library Manager App Online")
         self.setGeometry(600, 200 , 800, 600) # (x, y, width, height)
         self.setWindowIcon(QIcon("img/cup_of_coffee_logo_flat.png"))
+        self.buttonSearchBooks = QPushButton("Lancer la recherche", self)
         self.initUI()
         
     def initUI(self):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
-
         
         labelTitle = QLabel("Library Manager App Online", self)
         labelTitle.setFont(QFont("Arial", 20))
@@ -32,7 +32,6 @@ class MainWindow(QMainWindow):
         labelLibImg.setGeometry(0, 0, 800, 127)
         
         pixmap = QPixmap("img/library_image.jpg")
-        # pixmap = QPixmap("img/library_image_resized.jpg")
         labelLibImg.setPixmap(pixmap)
         labelLibImg.setScaledContents(True)
         labelLibImg.setGeometry((self.width() - labelLibImg.width()) // 2,
@@ -40,25 +39,42 @@ class MainWindow(QMainWindow):
                           labelLibImg.width(), 
                           labelLibImg.height())
         
-        labelWelcomeMessage = QLabel("Bienvenue ! Que recherchez vous ?", self)
+            # Welcome message
+        labelWelcomeMessage = QLabel("Bienvenue ! Quel livre recherchez vous ?", self)
         labelWelcomeMessage.setFont(QFont("Arial", 10))
         labelWelcomeMessage.setStyleSheet("color: #6a04c9;")
-          
         labelWelcomeMessage.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
         
         labelSearchInput = QLabel("HERE input field", self)
-        labelSearchButton = QLabel("HERE button to click to search", self)
         
+        # labelSearchButton = QLabel("HERE button to click to search", self)
         
+            # button display all books
+        self.buttonSearchBooks.setGeometry(450, 450, 200, 100)
+        self.buttonSearchBooks.setStyleSheet("font-size: 20px")
+        self.buttonSearchBooks.clicked.connect(self.displayAllBooks)
+        
+            # display results area
+        self.labelDisplayResults = QLabel("Résultats de recherche", self)
+        self.labelDisplayResults.setGeometry(150, 300, 200, 100)
+        self.labelDisplayResults.setStyleSheet("font-size: 30px")
+        
+            # LAYOUTS
         vbox = QVBoxLayout()
         vbox.addWidget(labelLibImg)
         vbox.addWidget(labelTitle)
         vbox.addWidget(labelWelcomeMessage)
         vbox.addWidget(labelSearchInput)
-        vbox.addWidget(labelSearchButton)
+        vbox.addWidget(self.buttonSearchBooks)
+        vbox.addWidget(self.labelDisplayResults)
+
         
         central_widget.setLayout(vbox)
         
+    def displayAllBooks(self): 
+        print("Button clicked")
+        self.labelDisplayResults.setText("Display list of books HERE")
+
         
 def main():
     app = QApplication(sys.argv)
